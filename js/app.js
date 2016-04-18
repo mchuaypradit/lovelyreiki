709b8e2d02835926726f74f56573cfe2c94919bc
MELANIE CHUAYPRADIT <melaniechuaypradit@MELANIEs-MBP.home>
2016-04-18 15:29:29 -0400

    add instafeed to inspire ssection and fixed contact height

--- js/app.js
+++ js/app.js
@@ -1,2 +1,21 @@
 $(document).foundation();
 
+$(document).ready(function() {
+        var userFeed = new Instafeed({
+        	get: 'user',
+        	userId: '3044589536',
+            accessToken: '3044589536.e8599b5.58beaa0fc4ac437682c34f388a432172',
+            target: 'instafeed',
+            links: true,
+            limit: 6,
+            sortBy: 'most-recent',
+            resolution: 'standard_resolution',
+            template: '<div class="column small-12 medium-4 large-4 instagram-box"><div class="photo-box"><a href="{{link}}"><img src="{{image}}"></a><div class="description">{{caption}}</div></div></div>'	
+            });
+         
+
+            userFeed.run();
+
+
+});
+
