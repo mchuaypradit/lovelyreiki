07f06b755cd2632493a4ee74388129066933cb89
MELANIE CHUAYPRADIT <melaniechuaypradit@MELANIEs-MBP.home>
2016-04-29 00:20:44 -0400

    Add more praises, fix padding and margins

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
             accessToken: '3044589536.e8599b5.58beaa0fc4ac437682c34f388a432172',
             target: 'instafeed',
             links: true,
-            limit: 3,
+            limit: 6,
             sortBy: 'most-recent',
             resolution: 'standard_resolution',
             template: '<div class="column small-12 medium-4 large-4 instagram-box"><div class="photo-box"><a href="{{link}}"><img src="{{image}}"></a><div class="description">{{caption}}</div></div></div>'	
