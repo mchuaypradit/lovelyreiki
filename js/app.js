3bd22e1db671f857d315c8db46807c1e8d0f236a
Jameson Edwards <jameson.edwards@gmail.com>
2017-06-13 13:55:07 -0400

    testing fb link

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-$(document).foundation()
+$(document).foundation();
+
+function openFBPage() {
+    setTimeout(function () { window.location = "https://www.facebook.com/christinalovelyreiki/"; }, 25);
+    window.location = "fb://page/1801675033395776";
+}
+
+var fbFooterElement = document.getElementById('fb-footer-element');
+fbFooterElement.onclick = openFBPage;
