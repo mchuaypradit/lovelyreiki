8486b9ee7c2e55f99131bbd8e2370b28118e29b3
Jameson Edwards <jameson.edwards@gmail.com>
2017-06-19 11:33:11 -0400

    Fixed focus bug with contact for reset button

--- js/app.js
+++ js/app.js
@@ -1,13 +1,20 @@
 $(document).foundation();
 
+// Open the Facebook app if installed (on mobile), else open website.
 function openFBLink() {
     setTimeout(function () { window.location = "https://www.facebook.com/christinalovelyreiki/"; }, 25);
     window.location = "fb://page/1801675033395776";
 }
 
+// Open the Instagram app if installed (on mobile), else open website.
 function openIGLink() {
     setTimeout(function () { window.location = "https://www.instagram.com/christinalovelyreiki/"; }, 25);
     window.location = "instagram://user?username=christinalovelyreiki";
+}
+
+// Focus on Name field in contact form after clicking the reset button.
+function contactFormFocus() {
+    document.getElementById('fullname').focus();
 }
 
 var fbFooterElement = document.getElementById('fb-footer-element');
@@ -15,3 +22,5 @@ fbFooterElement.onclick = openFBLink;
 
 var igFooterElement = document.getElementById('ig-footer-element');
 igFooterElement.onclick = openIGLink;
+
+document.getElementById('contact-form-reset').onclick = contactFormFocus;
